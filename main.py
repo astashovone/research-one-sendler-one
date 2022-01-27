77a559ad0e0194315050388bba029f478441091c
User511A <48769117+User511A@users.noreply.github.com>
2022-01-27 13:26:14 +0500

    update

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from flask import Flask
 import time
 import requests
 response = requests.get('http://192.168.31.40:5000/run')
 time.sleep(3000)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello! I am running in a docker container'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
